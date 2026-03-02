5511f9d1c086db7c5d40ecc9178ea9e34b347f85
vuki-dev.exe <78960429+vuki-devexe@users.noreply.github.com>
2026-03-02 21:00:03 +0100

    changed something

---
+++ TiledBG.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
+
+
+namespace Rayteam_Advantures.Tiled
+{
+    class TiledBG
+    {
+        public static void LoadMaps(String mapname) 
+        {
+            string jsonString = File.ReadAllText("./assets/data/maps/"+mapname+".tmj");
+            TiledMap map = JsonSerializer.Deserialize<TiledMap>(jsonString);
+        }
+    }
+    public class TiledMap
+    {
+        public int width { get; set; }
+        public int height { get; set; }
+        public List<TiledLayer> layers { get; set; }
+        //public List<TiledTileset> tilesets { get; set; }
+    }
+
+    public class TiledLayer
+    {
+        public string name { get; set; }
+        public int[] data { get; set; } // The tile IDs
+        public int width { get; set; }
+        public int height { get; set; }
+    }
+}
